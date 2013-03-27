7fa9e2d34838382f7b4e9f7c4ca14dc3858bb88d
Jeff <ampernand@gmail.com>
2013-03-27 11:36:54 -0400

    timeout should send part messages

--- ircd/user.py
+++ ircd/user.py
@@ -299,7 +299,7 @@ class User(base.BaseObject):
         call to time out the user and disconnect them
         '''
         self.dbg('timed out')
-        self.close()
+        self.close_when_done()
 
     def _rand_nick(self,l):
         nick =  base64.b32encode(os.urandom(l)).replace('=','')
